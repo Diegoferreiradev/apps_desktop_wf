bd0551d7d016c9da21897dc2208edabee1440523
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-07-05 22:29:00 -0300

    Programa que verifica o Período do Aluno

---
+++ MatriculaDoAluno/MatriculaDeAluno.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MatriculaDoAluno
+{
+    partial class MatriculaDeAluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnIdentificarCategoria = new System.Windows.Forms.Button();
+            this.lblCategoria = new System.Windows.Forms.TextBox();
+            this.txtAniversario = new System.Windows.Forms.TextBox();
+            this.txtNascimento = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Ano de Nascimento :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Categoria :";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(53, 12);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(354, 20);
+            this.txtNome.TabIndex = 4;
+            // 
+            // btnIdentificarCategoria
+            // 
+            this.btnIdentificarCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIdentificarCategoria.Location = new System.Drawing.Point(15, 94);
+            this.btnIdentificarCategoria.Name = "btnIdentificarCategoria";
+            this.btnIdentificarCategoria.Size = new System.Drawing.Size(170, 23);
+            this.btnIdentificarCategoria.TabIndex = 7;
+            this.btnIdentificarCategoria.Text = "Identificar Categoria";
+            this.btnIdentificarCategoria.UseVisualStyleBackColor = true;
+            this.btnIdentificarCategoria.Click += new System.EventHandler(this.btnIdentificarCategoria_Click);
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.BackColor = System.Drawing.Color.Yellow;
+            this.lblCategoria.Location = new System.Drawing.Point(280, 94);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(144, 20);
+            this.lblCategoria.TabIndex = 8;
+            // 
+            // txtAniversario
+            // 
+            this.txtAniversario.Location = new System.Drawing.Point(385, 55);
+            this.txtAniversario.Name = "txtAniversario";
+            this.txtAniversario.Size = new System.Drawing.Size(123, 20);
+            this.txtAniversario.TabIndex = 10;
+            // 
+            // txtNascimento
+            // 
+            this.txtNascimento.Location = new System.Drawing.Point(128, 55);
+            this.txtNascimento.Name = "txtNascimento";
+            this.txtNascimento.Size = new System.Drawing.Size(132, 20);
+            this.txtNascimento.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "&Ano Último Aniversário";
+            // 
+            // MatriculaDeAluno
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 131);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNascimento);
+            this.Controls.Add(this.txtAniversario);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.btnIdentificarCategoria);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MatriculaDeAluno";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matrícula do Aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnIdentificarCategoria;
+        private System.Windows.Forms.TextBox lblCategoria;
+        private System.Windows.Forms.TextBox txtAniversario;
+        private System.Windows.Forms.TextBox txtNascimento;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MatriculaDoAluno/MatriculaDeAluno.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace MatriculaDoAluno
+{
+    public partial class MatriculaDeAluno : Form
+    {
+        public MatriculaDeAluno()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAniversario_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNascimento.Text.Trim().Length < 4) {
+                MessageBox.Show("É preciso informar o Ano de Nascimento com 4 Digitos", "Atenção",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                txtNascimento.Focus();
+            }
+
+        }
+
+        private void txtAniversario_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtAniversario.Text != string.Empty && 
+                Convert.ToInt32(txtAniversario.Text) <= 
+                Convert.ToInt32(txtNascimento.Text)) {
+                MessageBox.Show("O Ano do último Aniversário deve ser superior ao do Ano de Nascimento.", 
+                    "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Cancel = true;
+            }
+        }
+
+        private void btnIdentificarCategoria_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == string.Empty ||
+                txtNascimento.Text == string.Empty ||
+                txtAniversario.Text == string.Empty)
+            {
+                MessageBox.Show("Todos os dados solicitados devem ser informados.", "Atenção",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            }
+            else
+            {
+                int idade = Convert.ToInt32(txtAniversario.Text) -
+                            Convert.ToInt32(txtNascimento.Text);
+                if (idade > 17)
+                {
+                    lblCategoria.Text = "Adulto";
+                }
+                else if (idade > 13)
+                {
+                    lblCategoria.Text = "Juvenil B";
+                }
+                else if (idade > 10)
+                {
+                    lblCategoria.Text = "Juvenil A";
+                }
+                else if (idade > 7)
+                {
+                    lblCategoria.Text = "Infantil B";
+                }
+                else if (idade > 5)
+                {
+                    lblCategoria.Text = "Infantil A";
+                }
+                else
+                {
+                    lblCategoria.Text = "Essa Categoria Não Existe!";
+                }
+
+            }
+        }
+
+    }
+}
