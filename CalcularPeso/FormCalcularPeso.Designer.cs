b7d212d105669361f9bd7dcbf5e59e0fa4d1c571
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-07-05 22:23:40 -0300

    Programa para Calcular o Peso da Pessoa

---
+++ CalcularPeso/FormCalcularPeso.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CalcularPeso
+{
+    partial class FormCalcularPeso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.rbnFeminino = new System.Windows.Forms.RadioButton();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblPesoIdeal);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtAltura);
+            this.groupBox1.Controls.Add(this.rbnFeminino);
+            this.groupBox1.Controls.Add(this.rbnMasculino);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 136);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Peso Ideal";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Altura :";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(196, 41);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 2;
+            this.txtAltura.TextChanged += new System.EventHandler(this.txtAltura_TextChanged);
+            // 
+            // rbnFeminino
+            // 
+            this.rbnFeminino.AutoSize = true;
+            this.rbnFeminino.Location = new System.Drawing.Point(92, 41);
+            this.rbnFeminino.Name = "rbnFeminino";
+            this.rbnFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbnFeminino.TabIndex = 1;
+            this.rbnFeminino.Text = "Feminino";
+            this.rbnFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rbnMasculino
+            // 
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Checked = true;
+            this.rbnMasculino.Location = new System.Drawing.Point(13, 41);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbnMasculino.TabIndex = 0;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            this.rbnMasculino.CheckedChanged += new System.EventHandler(this.rbnMasculino_CheckedChanged);
+            // 
+            // lblPesoIdeal
+            // 
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoIdeal.Location = new System.Drawing.Point(132, 100);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(40, 20);
+            this.lblPesoIdeal.TabIndex = 5;
+            this.lblPesoIdeal.Text = "72.7";
+            // 
+            // FormCalcularPeso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 137);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "FormCalcularPeso";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cálculo do Peso Ideal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.RadioButton rbnFeminino;
+        private System.Windows.Forms.RadioButton rbnMasculino;
+        private System.Windows.Forms.Label lblPesoIdeal;
+    }
+}
+
---
+++ CalcularPeso/FormCalcularPeso.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalcularPeso
+{
+    public partial class FormCalcularPeso : Form
+    {
+        RadioButton rbnSelecionado = null;
+
+        public FormCalcularPeso()
+        {
+            InitializeComponent();
+        }
+
+        private void rbnMasculino_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbn = (RadioButton)sender;
+
+            if (rbn.Checked)
+            {
+                rbnSelecionado = rbn;
+                SetPesoIdeal();
+            }
+        }
+
+        private void txtAltura_TextChanged(object sender, EventArgs e)
+        {
+            SetPesoIdeal();
+        }
+
+        private void SetPesoIdeal()
+        {
+            try
+            {
+                double altura = Convert.ToDouble(txtAltura.Text);
+                double pesoIdeal;
+                if (rbnSelecionado.Text.Equals("Masculino"))
+                    pesoIdeal = (72.7 * altura) - 58;
+                else
+                    pesoIdeal = (62.1 * altura) - 44.7;
+                lblPesoIdeal.Text = pesoIdeal.ToString("N");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Selecione o Sexo e informe a Altura corretamente", "Atenção!", 
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
