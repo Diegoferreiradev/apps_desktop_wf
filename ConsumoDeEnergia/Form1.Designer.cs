4b04c1d04a57e61351ab5e3cf073b406c449f039
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-07-05 22:25:19 -0300

    Programa para Calcular o Consumo de Energia

---
+++ ConsumoDeEnergia/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ConsumoDeEnergia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCasa = new System.Windows.Forms.TextBox();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dgvLeituras = new System.Windows.Forms.DataGridView();
+            this.btnProcessarDados = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeituras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N° Casa :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Consumo :";
+            // 
+            // txtCasa
+            // 
+            this.txtCasa.Location = new System.Drawing.Point(71, 27);
+            this.txtCasa.Name = "txtCasa";
+            this.txtCasa.Size = new System.Drawing.Size(100, 20);
+            this.txtCasa.TabIndex = 2;
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Location = new System.Drawing.Point(247, 27);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(100, 20);
+            this.txtConsumo.TabIndex = 3;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(376, 25);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(91, 23);
+            this.btnRegistrar.TabIndex = 4;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dgvLeituras
+            // 
+            this.dgvLeituras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLeituras.Location = new System.Drawing.Point(16, 59);
+            this.dgvLeituras.Name = "dgvLeituras";
+            this.dgvLeituras.Size = new System.Drawing.Size(451, 302);
+            this.dgvLeituras.TabIndex = 5;
+            // 
+            // btnProcessarDados
+            // 
+            this.btnProcessarDados.Location = new System.Drawing.Point(16, 386);
+            this.btnProcessarDados.Name = "btnProcessarDados";
+            this.btnProcessarDados.Size = new System.Drawing.Size(155, 23);
+            this.btnProcessarDados.TabIndex = 6;
+            this.btnProcessarDados.Text = "Processar Dados";
+            this.btnProcessarDados.UseVisualStyleBackColor = true;
+            this.btnProcessarDados.Click += new System.EventHandler(this.btnProcessarDados_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(264, 391);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(203, 13);
+            this.lblResultado.TabIndex = 7;
+            this.lblResultado.Text = "Total do consumo sem desconto : 350.00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 426);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnProcessarDados);
+            this.Controls.Add(this.dgvLeituras);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.txtCasa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consumo de Energia de um Condomínio";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeituras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCasa;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DataGridView dgvLeituras;
+        private System.Windows.Forms.Button btnProcessarDados;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
