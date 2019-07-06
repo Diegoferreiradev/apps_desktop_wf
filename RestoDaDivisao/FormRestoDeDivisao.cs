250fbc3a3feef120fadc452717dce8ab6a845331
Diegoferreiradev <diegoferreiradev@gmail.com>
2019-07-05 22:27:37 -0300

    Programa que exibe o resto de uma divisão

---
+++ RestoDaDivisao/FormRestoDeDivisao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RestoDaDivisao
+{
+    public partial class FormRestoDeDivisao : Form
+    {
+        public FormRestoDeDivisao()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+            int dividendo = Convert.ToInt32(txtDividendo.Text);
+            int divisor = Convert.ToInt32(txtDivisor.Text);
+            int restoDivisao = dividendo % divisor;
+            txtResto.Text = restoDivisao.ToString();
+        }
+    }
+}
